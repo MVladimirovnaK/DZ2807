1562a41a97e145a4163904342db745b985e2793b
mariya <mariyakudryashova19980308@gmail.com>
2023-07-14 11:30:36 +0300

    added sem1

--- Desktop/Обучение Разработчик/7. Знакомство с языками программирования (семинары)/4.07. Урок 1. Знакомство с языком программирования С#/seminar407/sem4071/obj/Debug/net6.0/sem4071.AssemblyInfo.cs
+++ Desktop/Обучение Разработчик/7. Знакомство с языками программирования (семинары)/4.07. Урок 1. Знакомство с языком программирования С#/seminar407/sem4071/obj/Debug/net6.0/sem4071.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("seminar407")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("sem4071")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("seminar407")]
-[assembly: System.Reflection.AssemblyTitleAttribute("seminar407")]
+[assembly: System.Reflection.AssemblyProductAttribute("sem4071")]
+[assembly: System.Reflection.AssemblyTitleAttribute("sem4071")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
