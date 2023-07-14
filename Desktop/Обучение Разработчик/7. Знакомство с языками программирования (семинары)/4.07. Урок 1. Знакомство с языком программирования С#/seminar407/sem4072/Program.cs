961b2fcbd191bce0bb0c6aafef2352d95bd618fa
mariya <mariyakudryashova19980308@gmail.com>
2023-07-14 09:51:12 +0300

    2zadacha

---
+++ Desktop/Обучение Разработчик/7. Знакомство с языками программирования (семинары)/4.07. Урок 1. Знакомство с языком программирования С#/seminar407/sem4072/Program.cs
@@ -0,0 +1,15 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+int numberA = new Random().Next(1, 100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+
+if (numberA == numberB * numberB)
+
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
